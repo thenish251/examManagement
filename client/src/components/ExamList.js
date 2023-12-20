8af4b701b5135072c1b35ec58cc263299421c5e7
thenish51 <danish@sosimtech.com>
2023-12-20 20:16:08 +0530

    edit examList

--- client/src/components/ExamList.js
+++ client/src/components/ExamList.js
@@ -6,13 +6,18 @@ const ExamList = () => {
   const [exams, setExams] = useState([]);
   const [newExamTitle, setNewExamTitle] = useState('');
   const [newExamDuration, setNewExamDuration] = useState('');
+  const [editingExamId, setEditingExamId] = useState(null);
   const [error, setError] = useState('');
 
   useEffect(() => {
+    fetchExams();
+  }, []);
+
+  const fetchExams = () => {
     axios.get('http://localhost:3000/exams')
       .then(response => setExams(response.data))
       .catch(error => console.error('Error fetching exams:', error));
-  }, []);
+  };
 
   const handleAddExam = () => {
     axios.post('http://localhost:3000/exams', {
@@ -33,8 +38,40 @@ const ExamList = () => {
 
   const handleDeleteExam = (examId) => {
     axios.delete(`http://localhost:3000/exams/${examId}`)
-      .then(() => setExams(exams.filter(exam => exam._id !== examId)))
+      .then(() => {
+        setExams(exams.filter(exam => exam._id !== examId));
+        setEditingExamId(null); // Reset editing state
+      })
       .catch(error => console.error('Error deleting exam:', error));
+  };
+
+  const handleEditExam = (examId) => {
+    setEditingExamId(examId);
+  };
+
+  const handleSaveEdit = () => {
+    const editedExam = {
+      title: newExamTitle,
+      duration: newExamDuration,
+    };
+
+    axios.put(`http://localhost:3000/exams/${editingExamId}`, editedExam)
+      .then(() => {
+        fetchExams(); // Fetch exams after editing
+        setEditingExamId(null); // Reset editing state
+        setNewExamTitle('');
+        setNewExamDuration('');
+      })
+      .catch(error => {
+        console.error('Error editing exam:', error);
+        setError('Error editing exam. Please try again.');
+      });
+  };
+
+  const handleCancelEdit = () => {
+    setEditingExamId(null);
+    setNewExamTitle('');
+    setNewExamDuration('');
   };
 
   return (
@@ -44,8 +81,30 @@ const ExamList = () => {
       <ul>
         {exams.map(exam => (
           <li key={exam._id}>
-            <span>{exam.title}</span> - <span>{exam.duration} minutes</span>
-            <button onClick={() => handleDeleteExam(exam._id)}>Delete</button>
+            {editingExamId === exam._id ? (
+              <div>
+                <input
+                  type="text"
+                  placeholder="Edit Title"
+                  value={newExamTitle}
+                  onChange={(e) => setNewExamTitle(e.target.value)}
+                />
+                <input
+                  type="number"
+                  placeholder="Edit Duration (minutes)"
+                  value={newExamDuration}
+                  onChange={(e) => setNewExamDuration(e.target.value)}
+                />
+                <button onClick={handleSaveEdit}>Save</button>
+                <button onClick={handleCancelEdit}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                <span>{exam.title}</span> - <span>{exam.duration} minutes</span>
+                <button onClick={() => handleEditExam(exam._id)}>Edit</button>
+                <button onClick={() => handleDeleteExam(exam._id)}>Delete</button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
--- client/src/components/McqList.js
+++ client/src/components/McqList.js
@@ -1,61 +1,113 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import './McqList.css';
+import './McqList.css'; // Import your CSS file
 
 const McqList = ({ examId }) => {
   const [mcqs, setMCQs] = useState([]);
   const [newMCQQuestion, setNewMCQQuestion] = useState('');
-  const [newMCQOptions, setNewMCQOptions] = useState([]);
+  const [newMCQOptions, setNewMCQOptions] = useState('');
   const [newMCQCorrectAnswer, setNewMCQCorrectAnswer] = useState('');
+  const [editingMCQ, setEditingMCQ] = useState(null);
 
-const fetchMCQs = useCallback(() => {
-  console.log(examId);
-  axios.get(`http://localhost:3000/mcqs/exam/${examId}`)
-    .then(response => setMCQs(response.data))
-    .catch(error => console.error('Error fetching MCQs:', error));
-}, [examId]);
+  const fetchMCQs = useCallback(() => {
+    axios.get(`http://localhost:3000/mcqs/exam/${examId}`)
+      .then(response => setMCQs(response.data))
+      .catch(error => console.error('Error fetching MCQs:', error));
+  }, [examId]);
 
   useEffect(() => {
+    // Fetch MCQs for a specific exam from the server on component mount
     fetchMCQs();
   }, [fetchMCQs, examId]);
 
+  const handleEditMCQ = (mcqId) => {
+    // Set the editingMCQ state to the MCQ ID being edited
+    setEditingMCQ(mcqId);
+  };
+
+  const handleSaveEdit = () => {
+    // Implement logic to save the edited MCQ
+    // This is a placeholder; you should customize it according to your requirements
+    console.log(`Save edited MCQ with ID: ${editingMCQ}`);
+    // Reset the editing state
+    setEditingMCQ(null);
+    // Fetch MCQs after editing
+    fetchMCQs();
+  };
+
+  const handleCancelEdit = () => {
+    // Cancel editing by resetting the editing state
+    setEditingMCQ(null);
+  };
+
   const handleAddMCQ = () => {
-    console.log('examId:',examId)
-    axios.post('http://localhost:3000/mcqs', {
+    // Implement logic to add a new MCQ for the specific exam
+    axios.post(`http://localhost:3000/mcqs/exam/${examId}/mcqs`, {
       question: newMCQQuestion,
-      options: newMCQOptions,
+      options: newMCQOptions.split(',').map(option => option.trim()),
       correctAnswer: newMCQCorrectAnswer,
-      examId: examId,
     })
       .then(response => {
         setMCQs([...mcqs, response.data]);
         setNewMCQQuestion('');
-        setNewMCQOptions([]);
+        setNewMCQOptions('');
         setNewMCQCorrectAnswer('');
       })
       .catch(error => console.error('Error adding MCQ:', error));
   };
 
   const handleDeleteMCQ = (mcqId) => {
+    // Implement logic to delete an MCQ
     axios.delete(`http://localhost:3000/mcqs/${mcqId}`)
       .then(() => {
         setMCQs(mcqs.filter(mcq => mcq._id !== mcqId));
+        // Reset editing state after deletion
+        setEditingMCQ(null);
       })
       .catch(error => console.error('Error deleting MCQ:', error));
   };
 
   return (
-    <div className="mcq-list-container">
+    <div>
       <h2>MCQ List for Exam {examId}</h2>
-      <ul className="mcq-list">
+      <ul>
         {mcqs.map(mcq => (
-          <li key={mcq._id} className="mcq-item">
+          <li key={mcq._id}>
             {mcq.question} - Correct Answer: {mcq.correctAnswer}
-            <button onClick={() => handleDeleteMCQ(mcq._id)}>Delete</button>
+            {editingMCQ === mcq._id ? (
+              <div>
+                {/* Modal for editing */}
+                <input
+                  type="text"
+                  placeholder="Edit Question"
+                  value={newMCQQuestion}
+                  onChange={(e) => setNewMCQQuestion(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Edit Options (comma-separated)"
+                  value={newMCQOptions}
+                  onChange={(e) => setNewMCQOptions(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Edit Correct Answer"
+                  value={newMCQCorrectAnswer}
+                  onChange={(e) => setNewMCQCorrectAnswer(e.target.value)}
+                />
+                <button onClick={handleSaveEdit}>Save</button>
+                <button onClick={handleCancelEdit}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={() => handleEditMCQ(mcq._id)}>Edit</button>
+                <button onClick={() => handleDeleteMCQ(mcq._id)}>Delete</button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
-      <div className="add-mcq-form">
+      <div>
         <h3>Add New MCQ</h3>
         <input
           type="text"
@@ -67,7 +119,7 @@ const fetchMCQs = useCallback(() => {
           type="text"
           placeholder="Options (comma-separated)"
           value={newMCQOptions}
-          onChange={(e) => setNewMCQOptions(e.target.value.split(','))}
+          onChange={(e) => setNewMCQOptions(e.target.value)}
         />
         <input
           type="text"
